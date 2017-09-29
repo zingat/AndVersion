2f2636459f23b9e625de8aee621389fb50987bd0
ismailgungor <birihtimaldahavar:D>
2017-09-29 15:37:52 +0300

    Added Constant value as "LAST_SESSION_VERSION"

--- andversion/src/main/java/com/zingat/andversion/constants/Constants.java
+++ andversion/src/main/java/com/zingat/andversion/constants/Constants.java
@@ -13,5 +13,7 @@ public class Constants {
     public static final String WHATSNEW_TR_ARRAY = "tr";
     public static final String WHATSNEW_EN_ARRAY = "en";
 
+    public static final String LAST_SESSION_VERSION = "lastSessionVersion";
+
 
 }
