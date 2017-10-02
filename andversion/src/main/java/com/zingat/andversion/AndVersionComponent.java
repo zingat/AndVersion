7b76ce20a32369f224af24fed03e4fe18698105a
ismailgungor <birihtimaldahavar:D>
2017-10-02 16:07:44 +0300

    Deleted unnecessary lines

--- andversion/src/main/java/com/zingat/andversion/AndVersionComponent.java
+++ andversion/src/main/java/com/zingat/andversion/AndVersionComponent.java
@@ -4,9 +4,6 @@ import javax.inject.Singleton;
 
 import dagger.Component;
 
-/**
- * Created by ismailgungor on 28.09.2017.
- */
 @Singleton
 @Component( modules = { AndVersionModule.class } )
 public interface AndVersionComponent {
