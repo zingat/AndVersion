2b75b4a0d6994866d4599ec0c89bc54ddd35776a
ismailgungor <birihtimaldahavar:D>
2017-10-03 14:26:13 +0300

    Removed constant string value as WHATSNEW_TR_ARRAY

--- andversion/src/main/java/com/zingat/andversion/constants/Constants.java
+++ andversion/src/main/java/com/zingat/andversion/constants/Constants.java
@@ -6,7 +6,6 @@ public class Constants {
     public static final String MIN_VERSION_OBJECT = "MinVersion";
     public static final String CURRENT_VERSION_OBJECT = "CurrentVersion";
     public static final String WHATSNEW_OBJECT = "WhatsNew";
-    public static final String WHATSNEW_TR_ARRAY = "tr";
     public static final String WHATSNEW_EN_ARRAY = "en";
 
     public static final String LAST_SESSION_VERSION = "lastSessionVersion";
